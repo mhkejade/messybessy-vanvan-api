fb697b193df57add91d763a0a64303f6bd105749
Mike Bengil <mike.bengil@gmail.com>
2021-08-11 21:43:52 +0800

    updated test for flake8 linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,10 +5,10 @@ from app.calc import add, subtract
 
 class CalcTests(TestCase):
 
-    def test_add_number(self):
+    def test_add_numbers(self):
         """test that 2 numbers are added"""
         self.assertEqual(add(3, 8), 11)
 
-    def test_subtract_number(self):
+    def test_subtract_numbers(self):
         """test that 2 numbers are subtracted"""
         self.assertEqual(subtract(7, 2), 5)
